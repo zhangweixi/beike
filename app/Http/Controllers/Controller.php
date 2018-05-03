06d0d344d62d7c8bd18f3b2b8fa91a83e38cb9e1
zhangweixi <zhweixi@sina.cn>
2018-05-03 18:12:03 +0800

    add action

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    
+    public function action(Request $request)
+    {
+    	$method	= $request->input('action');
+    	
+    	return $this->$method($request);
+    }
 }
