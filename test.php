7582c96cbb62b2c3b2b6ad424f53a7084e913a37
zhweixi@sina.cn <zhweixi@sina.cn>
2018-06-07 16:00:51 +0800

    tes1

---
+++ test.php
@@ -0,0 +1,10 @@
+#!E:\phpstudy\PHPTutorial\php\php-7.2.1-nts\php.exe
+<?php
+echo PHP_INT_MAX;
+exit;
+echo date('Y-m-d H:i:s',time());
+exit("\n");
+echo md5(md5('30zhangsanlanqi888'));
+exit;
+echo md5(md5(20180203));
+echo "\n\r";
