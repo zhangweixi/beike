3cc55ff3d89faef14e7077acd518df4ed7ddf471
zhweixi@sina.cn <zhweixi@sina.cn>
2018-06-26 15:05:52 +0800

    发

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',['prefix'=>"api/v1",'namespace'=>"App\Http\Controllers\Api\V1"],function ($api)
+$api->version('v1',['prefix'=>"api/v1",'middleware'=>['apiSign','saveApiData'],'namespace'=>"App\Http\Controllers\Api\V1"],function ($api)
 {
 
     $api->post("test","RestFulController@test");
@@ -12,10 +12,16 @@ $api->version('v1',['prefix'=>"api/v1",'namespace'=>"App\Http\Controllers\Api\V1
 
     $api->post('/match/uploadMatchData','MatchController@upload_match_data');
     $api->post('/match/{action}',       'MatchController@action');
-    //
 
-    //zhangweixi
+
     $api->post('/user/{action}',        'UserController@action');
+
+    //球场
+    $api->post('/court/{action}',       'CourtController@action');
+
+    $api->post('/device/{action}',      'DeviceController@action');
+
+    $api->post('/matlab/{action}',     'MatlabController@action');
 
 
 });
