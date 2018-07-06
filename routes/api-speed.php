00af901dd419c78c0d2af76c6b0501df645db4ab
zhweixi@sina.cn <zhweixi@sina.cn>
2018-07-07 00:21:13 +0800

    7

--- routes/api-speed.php
+++ routes/api-speed.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 
+
+Route::get('/api/speed/index/user','App\Http\Controllers\Speed\IndexController@user');
+
 $api = app('Dingo\Api\Routing\Router');
 //$api->version('v1',['prefix'=>"api/apeed",'middleware'=>['apiSign','saveApiData'],'namespace'=>"App\Http\Controllers\Api\V1"],function ($api)
+
 $api->version('v1',['prefix'=>"api/speed",'middleware'=>['saveApiData'],'namespace'=>"App\Http\Controllers\Speed"],function ($api)
 {
     $api->any('test',function(){return "helolo";});
