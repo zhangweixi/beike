f00602d419c594d2eae0def09c5e908e80a2f86e
zhweixi@sina.cn <zhweixi@sina.cn>
2018-07-06 15:22:57 +0800

    test time

--- app/Http/Controllers/Api/V1/HomeController.php
+++ app/Http/Controllers/Api/V1/HomeController.php
@@ -1,3 +1,27 @@
 <?php
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller{
+
+
+    /**
+     * app配置
+     * */
+    public function app_config(){
+
+        //1.APP版本
+
+        $versionIos        = "";
+        $versionAndroid    = "";
+
+
+        return apiData()
+            ->set_data('versionIos',$versionIos)
+            ->set_data('versionAndroid',$versionAndroid)
+            ->set_date('time',date('Y-m-d H:i:s'))
+            ->send();
+    }
+
+}
