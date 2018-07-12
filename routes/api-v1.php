bac29c59bc42c320eaff11fb524d781e7ff73d02
zhweixi@sina.cn <zhweixi@sina.cn>
2018-07-12 15:03:24 +0800

    no message

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -4,9 +4,8 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',['prefix'=>"api/v1",'middleware'=>['apiSign','saveApiData'],'namespace'=>"App\Http\Controllers\Api\V1"],function ($api)
+$api->version('v1',['prefix'=>"api/v1",'middleware'=>['saveApiData','apiSign'],'namespace'=>"App\Http\Controllers\Api\V1"],function ($api)
 {
-
     $api->post("test","RestFulController@test");
     $api->post("checkCode","RestFulController@check_mobile_code");
 
