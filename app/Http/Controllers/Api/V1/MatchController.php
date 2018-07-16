9193068adca956cd805e2344bc5facdf8a4ce386
zhweixi@sina.cn <zhweixi@sina.cn>
2018-07-16 15:20:22 +0800

    no message

--- app/Http/Controllers/Api/V1/MatchController.php
+++ app/Http/Controllers/Api/V1/MatchController.php
@@ -343,35 +343,40 @@ class MatchController extends Controller
         $matchModel = new MatchModel();
         $matchResult= $matchModel->get_match_result($matchId);
 
+        if($matchResult == null)
+        {
+            return apiData()->send(2001,"系统正在对数据玩命分析，请稍等");
+        }
+
         $data   = [
             'shoot'         => [
-                'speedMax'  => $matchResult->shoot_speed_max ??0,
-                'speedAvg'  => $matchResult->shoot_speed_avg ??0,
-                'disMax'    => $matchResult->shoot_dis_max??0,
-                'disAvg'    => $matchResult->shoot_dis_avg??0
+                'speedMax'  => $matchResult->shoot_speed_max,
+                'speedAvg'  => $matchResult->shoot_speed_avg,
+                'disMax'    => $matchResult->shoot_dis_max,
+                'disAvg'    => $matchResult->shoot_dis_avg
             ],
             'passShort'    => [
-                'speedMax'  => $matchResult->pass_s_speed_max??0,
-                'speedAvg'  => $matchResult->pass_s_speed_vag??0,
-                'disMax'    => $matchResult->pass_s_dis_max??0,
-                'disAvg'    => $matchResult->pass_s_dis_avg??0,
-                'number'    => $matchResult->pass_s_num??0
+                'speedMax'  => $matchResult->pass_s_speed_max,
+                'speedAvg'  => $matchResult->pass_s_speed_vag,
+                'disMax'    => $matchResult->pass_s_dis_max,
+                'disAvg'    => $matchResult->pass_s_dis_avg,
+                'number'    => $matchResult->pass_s_num
             ],
             'passLength'    => [
-                'speedMax'  => $matchResult->pass_l_speed_max??0,
-                'speedAvg'  => $matchResult->pass_l_speed_vag??0,
-                'disMax'    => $matchResult->pass_l_dis_max??0,
-                'disAvg'    => $matchResult->pass_l_dis_avg??0,
-                'number'    => $matchResult->pass_l_num??0
+                'speedMax'  => $matchResult->pass_l_speed_max,
+                'speedAvg'  => $matchResult->pass_l_speed_vag,
+                'disMax'    => $matchResult->pass_l_dis_max,
+                'disAvg'    => $matchResult->pass_l_dis_avg,
+                'number'    => $matchResult->pass_l_num
             ],
 
             'run'        => [
-                'lowDis'       => $matchResult->run_low_dis??0,
-                'lowTime'      => $matchResult->run_low_time??0,
-                'midDis'       => $matchResult->run_mid_dis??0,
-                'midTime'      => $matchResult->run_mid_time??0,
-                'highDis'       => $matchResult->run_high_dis??0,
-                'highTime'      => $matchResult->run_high_time??0
+                'lowDis'       => $matchResult->run_low_dis,
+                'lowTime'      => $matchResult->run_low_time,
+                'midDis'       => $matchResult->run_mid_dis,
+                'midTime'      => $matchResult->run_mid_time,
+                'highDis'       => $matchResult->run_high_dis,
+                'highTime'      => $matchResult->run_high_time
             ],
         ];
 
