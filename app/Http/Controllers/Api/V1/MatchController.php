9f83c7b60ddb0f42cc27d0d5a64f08c03e2446f2
zhweixi@sina.cn <zhweixi@sina.cn>
2018-08-17 14:07:07 +0800

    no message

--- app/Http/Controllers/Api/V1/MatchController.php
+++ app/Http/Controllers/Api/V1/MatchController.php
@@ -68,7 +68,7 @@ class MatchController extends Controller
         $matchModel     = new MatchModel();
         $sourceId       = $matchModel->add_match_source_data($matchData);
 
-        if($isAll == 1)
+        if($isAll == 1 || $foot == 'L')
         {
             return apiData()->send();
         }
