e4118819d6827054b58a8841c811ce5d7b6ecc2b
zhweixi@sina.cn <zhweixi@sina.cn>
2018-09-06 17:04:29 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     'filehost'     => env('FILE_HOST',''),
     'matlabhost'   => env('MATLAB_HOST',''),
     'apihost'      => env('API_HOST',''),
+    'wechatenv'    => env('WECHAT_ENV',false),
 
     /*
     |--------------------------------------------------------------------------
