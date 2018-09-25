d9fd0027e8dc5ccec468563208ad820b938d0f28
zhweixi@sina.cn <zhweixi@sina.cn>
2018-09-25 16:50:49 +0800

    no message

--- app/Http/Controllers/Api/V1/MatchController.php
+++ app/Http/Controllers/Api/V1/MatchController.php
@@ -483,7 +483,7 @@ class MatchController extends Controller
         $speedTimeMid   = 0;
         $speedTimeHigh  = 0;
         $staticTime     = 0;
-        $i              = 1;
+        $totalNum       = count($matches);
         foreach ($matches as $key => $match)
         {
             $totalPass += $match->pass;
@@ -498,7 +498,7 @@ class MatchController extends Controller
             $match->shoot   = $match->shoot ?? 0;
             $match->pass    = $match->pass ?? 0;
             $match->run     = $match->run ?? 0;
-            $match->x       = $i++;
+            $match->x       = $totalNum - $key;
             unset($match->run_high_time);
             unset($match->run_mid_time);
             unset($match->run_low_time);
