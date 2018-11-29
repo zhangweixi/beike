63ef034b7ede0e2c9e1322305d27bebe177d0e57
zhweixi@sina.cn <zhweixi@sina.cn>
2018-11-29 11:05:25 +0800

    no message

--- config/jpush.php
+++ config/jpush.php
@@ -2,5 +2,5 @@
 return [
     "appKey"    => "7418d1772b8d4021899efc21",
     "secret"    => "a59e64bb032e8ce3457792b7",
-    "logFile"	=> env('JPUSH_LOG_FILE',"logs/jpush.log")
+    "logFile"	=> env('JPUSH_LOG_FILE',public_path("logs/jpush.log"))
 ];
