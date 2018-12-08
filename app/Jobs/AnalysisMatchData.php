e8df9f409f4ff53dc3270bffd441d5a5bc9a20b6
zhangweixi.home <zhweixi@sina.cn>
2018-12-09 00:11:35 +0800

    no message

--- app/Jobs/AnalysisMatchData.php
+++ app/Jobs/AnalysisMatchData.php
@@ -1293,7 +1293,9 @@ class AnalysisMatchData implements ShouldQueue
         deldir($localDir);
         mk_dir($localDir);
 
-        $baseApiUrl     = config('app.apihost')."/uploads/match/{$matchId}/";
+        //$baseApiUrl     = config('app.apihost')."/uploads/match/{$matchId}/";
+        $baseApiUrl     = "http://172.19.48.1/uploads/match/{$matchId}/";
+
 
         //数据文件
         $baseSensorL    = "sensor-L.txt";
