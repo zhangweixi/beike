92abf40310c21357eb411a89fdd4fec6eadea8be
zhweixi@sina.cn <zhweixi@sina.cn>
2019-01-18 15:11:23 +0800

    no message

--- app/python/matlabsrc/Verification_speed.m
+++ app/python/matlabsrc/Verification_speed.m
@@ -54,3 +54,28 @@ for i = 1:m
         end
     end
 end
+% % 判断触球次数 
+% flag = zeros(1,3); speed3 = []; speed2 = []; speed1 = [];
+% for i = 1:length(output)
+%     if output(i,2) == 3 
+%         flag(1,1) = flag(1,1)+1;
+%         speed3(flag(1,1)) = output(i,7);
+%     end
+%     if output(i,2) == 2
+%         flag(1,2) = flag(1,2)+1;
+%         speed2(flag(1,2)) = output(i,7);
+%     end
+%     if output(i,2) == 1
+%         flag(1,3) = flag(1,3)+1;
+%         speed1(flag(1,3)) = output(i,7);
+%     end
+% end  
+% figure
+% title('触球速度柱状图');
+% subplot(3,1,1)
+% bar(speed1,'r'); xlabel('长传次数'); ylabel('m/s');
+% subplot(3,1,2)
+% bar(speed2,'k'); xlabel('短传次数'); ylabel('m/s');
+% subplot(3,1,3)
+% bar(speed3,'b'); xlabel('触球次数'); ylabel('m/s');
+end
