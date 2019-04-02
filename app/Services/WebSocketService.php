bd47769acb96bdee8399ba7fe87d26c8588ab4f6
zhweixi@sina.cn <zhweixi@sina.cn>
2019-04-02 16:35:42 +0800

    socket测试

--- app/Services/WebSocketService.php
+++ app/Services/WebSocketService.php
@@ -24,10 +24,7 @@ class WebSocketService implements WebSocketHandlerInterface{
         // 在触发onOpen事件之前Laravel的生命周期已经完结，所以Laravel的Request是可读的，Session是可读写的
         // \Log::info('New WebSocket connection', [$request->fd, request()->all(), session()->getId(), session('xxx'), session(['yyy' => time()])]);
 
-        //mylogger("新加入一个用户".$request->fd);
-        echo " a new user";
-	mylogger("this is a test");
-        $server->push($request->fd, 'Welcome to LaravelS');
+        $server->push($request->fd, '欢迎加入漕河泾足浴群');
 
         // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
     }
@@ -35,14 +32,19 @@ class WebSocketService implements WebSocketHandlerInterface{
     public function onMessage(Server $server, Frame $frame)
     {
         // \Log::info('Received message', [$frame->fd, $frame->data, $frame->opcode, $frame->finish]);
+        echo "收到:".$frame->data."\n";
+
         $server->push($frame->fd, date('Y-m-d H:i:s'));
-        // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
+        foreach($server->connections as $fd){
+            $server->push($fd,$frame->data);
+        }
+
 
     }
 
 
     public function onClose(Server $server, $fd, $reactorId)
     {
-    	echo "bye bye ,i'm closed ";    
+    	echo "\n ".$fd."bye bye ,i'm closed\n";
     }
 }
