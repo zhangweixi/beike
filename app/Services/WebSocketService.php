37100abdc4b78a4af214f64349618cac03311f6e
zhweixi@sina.cn <zhweixi@sina.cn>
2019-05-14 15:41:37 +0800

    no message

--- app/Services/WebSocketService.php
+++ app/Services/WebSocketService.php
@@ -7,12 +7,15 @@
  */
 namespace App\Services;
 
+use App\Models\Base\BaseUserSocketModel;
 use Hhxsv5\LaravelS\Swoole\WebSocketHandlerInterface;
 use Swoole\Http\Request;
 use Swoole\WebSocket\Frame;
 use Swoole\WebSocket\Server;
 
 class WebSocketService implements WebSocketHandlerInterface{
+
+    private $errMsg     = "";
 
     public function __construct()
     {
@@ -21,6 +24,10 @@ class WebSocketService implements WebSocketHandlerInterface{
 
     public function onClose(Server $server, $fd, $reactorId)
     {
+        $socket     = new BaseUserSocketModel();
+        $sockInfo   = $socket->detail($fd);
+        $socket->unconnect($fd);
+        jpush_content("网络状态通知","用户".$sockInfo->user_id."网络已断开:",6002,1,"15000606942");
         echo "\n ".$fd."bye bye ,i'm closed\n";
     }
 
@@ -28,13 +35,9 @@ class WebSocketService implements WebSocketHandlerInterface{
     {
         // 在触发onOpen事件之前Laravel的生命周期已经完结，所以Laravel的Request是可读的，Session是可读写的
         // \Log::info('New WebSocket connection', [$request->fd, request()->all(), session()->getId(), session('xxx'), session(['yyy' => time()])]);
-        $data = ['name'=>"足浴店老板",'content'=>'欢迎加入漕河泾足浴群'];
-
-        echo $request->fd."\n";
-
-        $server->push($request->fd, \GuzzleHttp\json_encode($data));
-
-        // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
+        echo "wellcome:".$request->fd;
+        $socket = new BaseUserSocketModel();
+        $socket->connect($request->fd);
     }
 
     public function onMessage(Server $server, Frame $frame)
@@ -43,15 +46,32 @@ class WebSocketService implements WebSocketHandlerInterface{
         //1.数据必须是JSON格式
 
         $data   = $frame->data;
+        $data   = $this->check($data);
 
-        echo $data."\n";
+        if($data === false){
+
+            $server->push($frame->fd,$this->errMsg);
+
+        }else{
+
+            switch ($data->action){
+
+                case "test":
+                    $this->test($server,$frame,$data);                      break;
+
+                case "match/markUserId":    //设备确认身份
+                    $this->online_inform($server,$frame,$data);             break;
+            }
+        }
+    }
+
+    public function check($data){
 
         if(!isJson($data)){
 
             $data   = ["code"=>5000,"msg"=>"数据不是json格式"];
-            $server->push($frame->fd,\GuzzleHttp\json_encode($data));
-
-           return false;
+            $this->errMsg = \GuzzleHttp\json_encode($data);
+            return false;
         }
 
         $data   = \GuzzleHttp\json_decode($data);
@@ -60,27 +80,23 @@ class WebSocketService implements WebSocketHandlerInterface{
         if(!isset($data->action)){
 
             $data   = ["code"=>5000,"msg"=>"必须包含action字段"];
-            $server->push($frame->fd,\GuzzleHttp\json_encode($data));
+            $this->errMsg   = \GuzzleHttp\json_encode($data);
             return false;
         }
 
-        switch ($data->action){
-
-            case "test":
-                $this->test($server,$frame,$data);                      break;
-
-            case "match/markUserId":
-                $this->online_inform($server,$frame,$data);             break;
-        }
+        return $data;
     }
-
 
     /**
      * 设备联网上线通知
      * */
     public function online_inform($server,$frame,$data){
 
-        mylogger($frame->data);
+        $socket     = new BaseUserSocketModel();
+        $socket->bind($frame->fd,$data->userId,"device");
+
+        //通知用户，联网成功
+        jpush_content("联网通知","用户".$data->userId."联网成功".$data->foot,6001,1,"15000606942");
 
     }
     /**
