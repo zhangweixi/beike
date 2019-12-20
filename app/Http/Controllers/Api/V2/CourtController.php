42e953612e9d6f8a856aced3e6347ea5b695eddf
zhweixi@sina.cn <zhweixi@sina.cn>
2019-12-20 18:27:37 +0800

    no message

--- app/Http/Controllers/Api/V2/CourtController.php
+++ app/Http/Controllers/Api/V2/CourtController.php
@@ -62,24 +62,27 @@ class CourtController extends Controller
             'created_at'    => date_time()
         ];
 
-        //DB::table('football_court_point')->insert($gpsPoint);
-        $prePosition = ["B"=>"A","C"=>"B","D"=>"C","E"=>"D","F"=>"E","G"=>"F"];
+        DB::table('football_court_point')->insert($gpsPoint);
 
-        if($position != "A" && Court::check_gps_group_num($prePosition[$position],$gpsGroupId) == false)
-        {
-            return apiData()->send(2004,"测得太快啦，返回上一点重测吧");
+        if(0){
+
+            $prePosition = ["B"=>"A","C"=>"B","D"=>"C","E"=>"D","F"=>"E","G"=>"F"];
+            if($position != "A" && Court::check_gps_group_num($prePosition[$position],$gpsGroupId) == false)
+            {
+                return apiData()->send(2004,"测得太快啦，返回上一点重测吧");
+            }
+
+            if($position == "G")//结束点
+            {
+                //将数据迁移到数据库
+                $gps = Court::get_gps_group_cache($gpsGroupId);
+                Court::remove_gps_group_cache($gpsGroupId);
+                DB::table('football_court_point')->insert($gps);
+                return apiData()->send();
+            }
+            Court::set_gps_group_cache($gpsPoint);
         }
 
-        if($position == "G")//结束点
-        {
-            //将数据迁移到数据库
-            $gps = Court::get_gps_group_cache($gpsGroupId);
-            Court::remove_gps_group_cache($gpsGroupId);
-            DB::table('football_court_point')->insert($gps);
-            return apiData()->send();
-        }
-
-        Court::set_gps_group_cache($gpsPoint);
         //3检查手机的GPS和设备的GPS的距离
         $msg    = $gpsInfo['lat'] ? "GPS无效":"偏差". gps_distance($lon,$lat,$gpsInfo['lon'],$gpsInfo['lat']);
 
