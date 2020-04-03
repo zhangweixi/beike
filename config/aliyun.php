2df029ae21f08d42551238deccfa3be8eff997d0
zhweixi@sina.cn <zhweixi@sina.cn>
2020-04-03 17:00:55 +0800

    修改配置

--- config/aliyun.php
+++ config/aliyun.php
@@ -1,9 +1,9 @@
 <?php
 return [
 
-    "appKey"        => "LTAIEfSrHz4wQo79",
-    "appSecret"     => "B3nKgqgbT9iHisk5DRAGpWB0MLdAYJ",
-    "appCode"       => "bd1fa9e4a44c441c8b5735c7047ecbbf",
+    "appKey"        => env("ALIYUN_APP_KEY"),
+    "appSecret"     => env("ALIYUN_APP_SECRET"),
+    "appCode"       => env("ALIYUN_APP_CODE"),
     "signName"      => "百科足球",
     "loginTempId"   => "SMS_148594054",
     "matchInviteId" => "SMS_143716614",
